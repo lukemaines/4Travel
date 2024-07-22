5055ccb0e05f8e98b58018e39d54b25f3e35f90a
Pritam Sur <sur.pritam9878@gmail.com>
2024-07-22 19:36:35 -0400

    Add Trip model

---
+++ models/Trip.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+Trip.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    trip_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    trip_origin: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    destination: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    start_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    end_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    budget: {
+      type: DataTypes.DECIMAL(10, 2),
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'trip',
+  }
+);
+
+module.exports = Trip;
