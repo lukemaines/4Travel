16db4f6346d7965a391e81ff7db0e6678e7f9f53
l.maines <lmmaines28@gmail.com>
2024-07-25 01:06:41 -0400

    added table relationships to index

--- models/User.js
+++ models/User.js
@@ -61,15 +61,5 @@ User.init(
         modelName: 'user',
     }
 );
-// eager loading table relationships between user and trips tables
-User.hasMany(Trip, {
-    foreignKey: 'userId',
-    // the user's trips will also be deleted if we delete the user
-    onDelete: 'CASCADE',
-});
-
-Trip.belongsTo(User, {
-    foreignKey: 'userId',
-});
 
 module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,12 +1,14 @@
 const User = require('./User');
 
-// User.hasMany(Project, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+// eager loading table relationships between user and trips tables
+User.hasMany(Trip, {
+    foreignKey: 'userId',
+    // the user's trips will also be deleted if we delete the user
+    onDelete: 'CASCADE',
+});
 
-// Project.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
+Trip.belongsTo(User, {
+    foreignKey: 'userId',
+});
 
 module.exports = { User };
